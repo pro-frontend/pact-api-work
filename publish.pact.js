63a1e7db8b5dacd6833f80097b92d25102227e9b
Dzmitryj Ivchenkov <dzmitryj.ivchenkov@gmail.com>
2024-05-16 14:52:31 +0300

    Initial commit
    add first test
    
    delete unused files
    
    update tests, gitignore, scripts
    
    Update .gitignore
    
    delete unused files
    
    update tests, gitignore, scripts

--- publish.pact.js
+++ publish.pact.js
@@ -1,24 +1,33 @@
 const pact = require('@pact-foundation/pact-node');
+const execSync = require('child_process').execSync;
 
-const pactBrokerUrl = process.env.PACT_BROKER_BASE_URL.trim();
-const pactBrokerToken = process.env.PACT_BROKER_TOKEN.trim();
+// Получение переменных окружения с проверкой на их наличие
+const pactBrokerUrl = process.env.PACT_BROKER_BASE_URL;
+const pactBrokerToken = process.env.PACT_BROKER_TOKEN;
+const publishVersion = process.env.PACT_BROKER_PUBLISH_VERSION;
 
-const gitHash = require('child_process').execSync('git rev-parse --short HEAD').toString().trim();
+if (!pactBrokerUrl || !pactBrokerToken) {
+  console.error('Ошибка: Необходимо задать PACT_BROKER_BASE_URL, PACT_BROKER_TOKEN и PACT_BROKER_PUBLISH_VERSION как переменные окружения.');
+  process.exit(1); // Завершаем выполнение с ошибкой
+}
+
+// Получение хэша последнего коммита из Git для использования как версии потребителя
+const gitHash = execSync('git rev-parse --short HEAD').toString().trim();
 
 const opts = {
-	pactFilesOrDirs: ['./pacts/'],
-	pactBroker: pactBrokerUrl,
-	pactBrokerToken: pactBrokerToken,
-	tags: ['test'],
-	consumerVersion: '0.1.1',
+  pactFilesOrDirs: ['./pacts/'],
+  pactBroker: pactBrokerUrl.trim(),
+  pactBrokerToken: pactBrokerToken.trim(),
+  tags: ['test'],
+  consumerVersion: publishVersion,
 };
 
 console.log('--------------opts: ', opts);
 
 pact.publishPacts(opts).then(() => {
-	console.log('Pact contract publish complete!');
-	console.log(`Head over to ${pactBrokerUrl}`);
-	console.log('to see your published contracts');
+  console.log('Pact contract publish complete!');
+  console.log(`Head over to ${pactBrokerUrl}`);
+  console.log('to see your published contracts');
 }).catch((e) => {
-	console.error('Pact contracts publishing failed: ', e);
+  console.error('Pact contracts publishing failed: ', e);
 });
