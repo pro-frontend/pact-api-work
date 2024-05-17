1f6578ea6b5c9ab66fe4e8fafd7704d0793a67cb
Dzmitryj Ivchenkov <dzmitryj.ivchenkov@gmail.com>
2024-05-17 15:54:30 +0300

    delete unused files

--- publish.pact.js
+++ publish.pact.js
@@ -7,27 +7,27 @@ const pactBrokerToken = process.env.PACT_BROKER_TOKEN;
 const publishVersion = process.env.PACT_BROKER_PUBLISH_VERSION;
 
 if (!pactBrokerUrl || !pactBrokerToken) {
-	console.error('Ошибка: Необходимо задать PACT_BROKER_BASE_URL и PACT_BROKER_TOKEN как переменные окружения.');
-	process.exit(1); // Завершаем выполнение с ошибкой
+  console.error('Ошибка: Необходимо задать PACT_BROKER_BASE_URL, PACT_BROKER_TOKEN и PACT_BROKER_PUBLISH_VERSION как переменные окружения.');
+  process.exit(1); // Завершаем выполнение с ошибкой
 }
 
 // Получение хэша последнего коммита из Git для использования как версии потребителя
 const gitHash = execSync('git rev-parse --short HEAD').toString().trim();
 
 const opts = {
-	pactFilesOrDirs: ['./pacts/'],
-	pactBroker: pactBrokerUrl.trim(),
-	pactBrokerToken: pactBrokerToken.trim(),
-	tags: ['test'],
-	consumerVersion: publishVersion,
+  pactFilesOrDirs: ['./pacts/'],
+  pactBroker: pactBrokerUrl.trim(),
+  pactBrokerToken: pactBrokerToken.trim(),
+  tags: ['test'],
+  consumerVersion: publishVersion,
 };
 
 console.log('--------------opts: ', opts);
 
 pact.publishPacts(opts).then(() => {
-	console.log('Pact contract publish complete!');
-	console.log(`Head over to ${pactBrokerUrl}`);
-	console.log('to see your published contracts');
+  console.log('Pact contract publish complete!');
+  console.log(`Head over to ${pactBrokerUrl}`);
+  console.log('to see your published contracts');
 }).catch((e) => {
-	console.error('Pact contracts publishing failed: ', e);
+  console.error('Pact contracts publishing failed: ', e);
 });
